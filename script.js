9b04b47b44d890f3d5c37c2206357404d346cc9a
azra466 <e255118@edu.sbl.ch>
2024-11-13 15:33:01 +0000

    Aufgabe 12

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ export function aufgabe01(args) {
 
   // Wir erzeugen hier eine leere Liste, in der wir das Resultat Stück für Stück anhängen.
   const result = []
+  //leere Zeichenkette
 
   // Mit dieser Schlaufe nummerieren wir jedes Zeichen in `input` durch. Das
   // machen wir um jedes Zeichen einzeln anzuschauen.
@@ -83,6 +84,45 @@ export function aufgabe03(args) {
 
 linkupExerciseHandler("[data-click=aufgabe03]", aufgabe03)
 
+export function aufgabe05(args) {
+  const input = args
+  const result = []
+
+  let hasUpperCaseLetter = false
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    const UpperCase = currentElement.toUpperCase()
+    if (currentElement === "." || currentElement === " ") {
+    } else if (currentElement === UpperCase) {
+      hasUpperCaseLetter = true
+    }
+  }
+  return hasUpperCaseLetter
+}
+
+linkupExerciseHandler("[data-click=aufgabe05]", aufgabe05)
+
+export function aufgabe06(args) {
+  const input = args
+  const result = []
+
+  let SpecialLetter = false
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    const UpperCaseLetter = currentElement.toUpperCase()
+
+    const LowerCaseLetter = UpperCaseLetter.toLowerCase()
+    if (currentElement === " ") result.push(currentElement)
+    else if (LowerCaseLetter === UpperCaseLetter) {
+      SpecialLetter = true
+    }
+  }
+  return SpecialLetter
+}
+linkupExerciseHandler("[data-click=aufgabe06]", aufgabe06)
+
 export function aufgabe08(args) {
   //func und tab
   const input = args
@@ -116,16 +156,45 @@ export function aufgabe09(args) {
   const result = []
 
   const len = input.length
-  if (===6) {
-
+  if (len === 6) {
+    // wenn Länge 6
     return true
-
+  } else {
+    return false
   }
-  for (let i = 0; i < input.length; i++) {
-    const currentElement = input[i]
-  }
-
-  return result.join("")
 }
 
 linkupExerciseHandler("[data-click=aufgabe09]", aufgabe09)
+export function aufgabe12(args) {
+  const input = args
+  const result = []
+
+  for (let i = 0; i < input.length; i++) {
+    // Suche die Position des ersten es in einem Text
+    if (input[i] === "e" || input[i] === "E") {
+      return i
+    } else {
+      return -1
+    }
+  }
+  return
+}
+
+linkupExerciseHandler("[data-click=aufgabe12]", aufgabe12)
+
+
+export function aufgabe12(args) {
+  const input = args
+  const result = []
+//ist Position = erstes "e"
+
+
+
+
+
+
+linkupExerciseHandler("[data-click=aufgabe13]", aufgabe13)
+
+export function aufgabe13(args) {
+  const input = args
+  const result = []
